69181926e07c63636c81e172e3143b593e59d4a7
CheyenneH <32414858+CheyenneH@users.noreply.github.com>
2021-05-03 20:47:26 +0200

    Update rhea_agent.py

--- rhea_agent.py
+++ rhea_agent.py
@@ -1,11 +1,13 @@
 from copy import deepcopy
 import random
 import itertools
+import map
+import time
 
 
 class RHEA_agent:
     # set random seed
-    random.seed(10)
+    #random.seed(10)
 
     def __init__(self, colour):
         self.colour = colour
@@ -17,6 +19,7 @@ class RHEA_agent:
         self.elite = 3
         self.tournament_size = 5
         self.maxMoves = 3
+        self.actions = None
 
     def get_possible_moves(self, owned_countries, map):
         """Get all the possible moves the agent could make"""
@@ -32,20 +35,177 @@ class RHEA_agent:
 
         return possible_moves
 
-    def performAction(self, action, owned_countries):
-        # assuming copies of the objects are given when necessary
-        from_country = action[0]
-        to_country = action[1]
-        number = action[2]
+    def adjust_map(self, player1_owned_countries, player2_owned_countries, state, map):
+        """Changes the map to reflect the owned countries and army counts as per the players"""
+        #for country in player1_owned_countries.keys():
+        for country, value in player1_owned_countries.items():
+            map.change_colour(str(country), 'b')
+            map.set_army_count(str(country), player1_owned_countries[str(country)])
 
-        # add to new country
-        if (to_country in owned_countries):
-            owned_countries[str(to_country)] += number
+        #for country in player2_owned_countries.keys():
+        for country, value in player2_owned_countries.items():
+            map.change_colour(str(country), 'r')
+            map.set_army_count(str(country), player2_owned_countries[str(country)])
+        return
+
+    def performAction(self, action, owned_countries, map, state):
+        """Takes a single move and performs it on the map graph, a move consists
+                of (origin_country, destination_country, armies_to_move)"""
+
+        if self.colour == 'b':
+            attacking_player_colour = 'b'
+            """If destination_country isn't owned by either player"""
+            if map.get_colours_dict()[action[1]] == 'k':
+                defending_player_colour = 'k'
+            elif map.get_colours_dict()[action[1]] == 'b':
+                defending_player_colour = None
+            else:
+                defending_player_colour = 'r'
         else:
-            owned_countries[str(to_country)] = number
+            attacking_player_colour = 'r'
+            """destination_country isn't owned by either player"""
+            if map.get_colours_dict()[action[1]] == 'k':
+                defending_player_colour = 'k'
+            elif map.get_colours_dict()[action[1]] == 'r':
+                defending_player_colour = None
+            else:
+                defending_player_colour = 'b'
+        """Check that the player still owns the origin_country, if it does not, 
+        then return without making the move."""
+        if map.get_colours_dict()[action[0]] != str(self.colour):
+            return
 
-        # subtract from old country if it's still there
-        owned_countries[str(from_country)] -= number
+        """Check if the origin country still has all the armies the move wants to
+        utilise"""
+
+        """If the origin_country does not have as many armies as the move
+        requires, then just use however many armies there are left."""
+        if map.get_armies_dict()[action[1]] >= action[2]:
+            attacking_player_armies = action[2]
+        else:
+            attacking_player_armies = map.get_armies_dict()[action[0]]
+
+        """If the same player owns both countries, just transfer the armies"""
+        if map.get_colours_dict()[action[0]] == map.get_colours_dict()[action[1]]:
+            origin_army_count = map.get_armies_dict()[action[0]]
+            destination_army_count = map.get_armies_dict()[action[0]]
+
+            map.set_army_count(action[0], (origin_army_count - attacking_player_armies))
+            map.set_army_count(action[1], (destination_army_count + attacking_player_armies))
+            #             attacking_player.add_owned_country(str(move[0]), (origin_army_count - attacking_player_armies))
+            #             attacking_player.add_owned_country(str(move[1]), (destination_army_count + attacking_player_armies))
+            state.update({action[1]: destination_army_count + attacking_player_armies})
+            state[action[0]] = origin_army_count - attacking_player_armies
+
+            return
+
+        """Check if defending country is a player, and not a black node, if it
+        is a player, they will have an associate owned_countries dict which has
+        to be adjusted"""
+        if defending_player_colour == 'r':
+            defending_player_owned_countries = {}
+            armies = map.get_armies_dict()
+            countries_colour = map.get_colours_dict()
+            for i in range(len(countries_colour)):
+                key = list(countries_colour.keys())[i]
+                if countries_colour[key] == 'r':
+                    defending_player_owned_countries.update({key: armies[key]})
+
+            defending_player_armies = defending_player_owned_countries[action[1]]
+        elif defending_player_colour == 'b':
+            defending_player_owned_countries = {}
+            armies = map.get_armies_dict()
+            countries_colour = map.get_colours_dict()
+            for i in range(len(countries_colour)):
+                key = list(countries_colour.keys())[i]
+                if countries_colour[key] == 'b':
+                    defending_player_owned_countries.update({key: armies[key]})
+            defending_player_armies = defending_player_owned_countries[action[1]]
+        else:
+            """If the country isn't owned by anyone, it has 2 armies"""
+            defending_player_armies = map.get_armies_dict()[action[1]]
+
+        """For every army attacking, get outcome of a single attack"""
+        for i in range(attacking_player_armies):
+            """If there are still defending armies in destination_country"""
+            if map.get_armies_dict()[action[1]] > 0:
+
+                defender_chance = random.randint(0, 100)  # <= 70 means defender wins
+                attacker_chance = random.randint(0, 100)  # <= 60 means attacker wins
+
+                """If both players successfully roll, nothing happens"""
+
+                """If defender successfully rolls but attacker doesnt, kill one
+                attacking army, defending army stays the same"""
+                if defender_chance <= 70 and attacker_chance > 60:
+                    new_value = state[action[0]] - 1
+                    attacking_player_armies -= 1  # may be able to remove this line entirely
+                    state[action[0]] = new_value
+                    #                     attacking_player.add_owned_country(str(move[0]), new_value)
+                    map.set_army_count(action[0], new_value)
+
+                """If attacking army successfully rolls but defender doesnt, kill one
+                defending army, attacking army stays the same"""
+                if defender_chance > 70 and attacker_chance <= 60:
+                    if defending_player_colour == 'r' or defending_player_colour == 'b':
+
+                        new_value = map.get_armies_dict()[action[1]] - 1
+
+                        defending_player_owned_countries.update({action[1]: new_value})
+
+                        map.set_army_count(action[1], new_value)
+
+
+                    else:
+                        """No one owns the defending country, so just remove one army in the map object"""
+                        value = map.get_armies_dict()[action[1]] - 1
+                        map.set_army_count(action[1], value)
+
+            """Else there are no defending armies anymore, flip country to attacking player and
+            add the remaining armies to it"""
+            if map.get_armies_dict()[action[1]] == 0:
+                state.update({action[1]: attacking_player_armies})
+
+                map.set_army_count(action[1], attacking_player_armies)
+                map.change_colour(action[1], self.colour)
+
+                """If destination_country is owned by the opposing player"""
+                if defending_player_colour == 'r' or defending_player_colour == 'b':
+                    defending_player_owned_countries.pop(action[1])
+
+            if defending_player_armies < 0:
+                print('Army count has gone below 0...')
+
+        """Adjust the game map to reflect the new game state after the move"""
+        if defending_player_colour == 'r':
+            self.adjust_map(state, defending_player_owned_countries, state, map)
+        elif defending_player_colour == 'b':
+            self.adjust_map(defending_player_owned_countries, state, map)
+        else:
+            self.adjust_map(state, {}, state, map)
+
+        return
+
+    def score_additional_armies(self, owned_countries):
+        total_armies = 5
+        map_graph = map.Map()
+        region_dict = map_graph.regions_dict
+        countries_owned_by_player = owned_countries
+
+        """Check if player owns any entire regions, allocate extra armies if they do"""
+        for region in region_dict.keys():
+            total_countries_in_region = len(region)
+            player_countries_count_in_region = 0
+            for country in region:
+                if str(country) in countries_owned_by_player.keys():
+                    player_countries_count_in_region += 1
+            """If the player owns all the countries in a region, they get a bonus"""
+            if player_countries_count_in_region == total_countries_in_region:
+                regions_bonus_dict = map_graph.get_bonus_dict()
+                bonus = regions_bonus_dict[str(region)]
+                total_armies += bonus
+
+        return total_armies
 
     def evaluateIndividual(self, individual, map, owned_countries):
         # make copy of the map and owned countries
@@ -56,11 +216,14 @@ class RHEA_agent:
             action = individual[i]
             possible_moves = self.get_possible_moves(owned_countries_copy, map_copy)
             if action in possible_moves:
-                self.performAction(action, owned_countries_copy)
+                self.performAction(action, owned_countries_copy, map_copy,  owned_countries_copy)
             else:
                 # random action
-                action = possible_moves[random.randint(0, len(possible_moves) - 1)]
-                self.performAction(action, owned_countries_copy)
+                if len(possible_moves) >= 1:
+                    action = possible_moves[random.randint(0, len(possible_moves) - 1)]
+                else:
+                    return -100
+                self.performAction(action, owned_countries_copy, map_copy,  owned_countries_copy)
             # check if gameover
             if len(owned_countries_copy) == 0:
                 return -100
@@ -69,7 +232,10 @@ class RHEA_agent:
         # number of countries
         fitness = len(owned_countries_copy)
 
-        return fitness
+        #score for additional armies
+        add_score = self.score_additional_armies(owned_countries)
+
+        return fitness + (5*add_score)
 
     def evaluateAction(self, action, map, owned_countries):
         # make copy of the map and owned countries
@@ -77,7 +243,7 @@ class RHEA_agent:
         owned_countries_copy = deepcopy(owned_countries)
 
         # do action
-        self.performAction(action, owned_countries_copy)
+        self.performAction(action, owned_countries_copy, map_copy,  owned_countries_copy)
 
         # evaluate
         fitness = len(owned_countries_copy)
@@ -99,7 +265,7 @@ class RHEA_agent:
                 action = possible_actions[n]
                 individual.append(action)
                 # do action
-                self.performAction(action, owned_countries_copy)
+                self.performAction(action, owned_countries_copy, map_copy,  owned_countries_copy)
 
             else:
                 best_score = 0
@@ -115,7 +281,7 @@ class RHEA_agent:
                 individual.append(best_action)
 
                 # do action
-                self.performAction(best_action, owned_countries_copy)
+                self.performAction(best_action, owned_countries_copy, map_copy,  owned_countries_copy)
 
         return individual
 
@@ -199,6 +365,8 @@ class RHEA_agent:
         return new_population, new_population_scores
 
     def choose_moves(self, map, colour, owned_countries, reinf_card_count):
+        self.map = map
+        t0 = time.clock()
         print(str(self.colour) + ' owns these countries: ' + str(owned_countries))
         # begin here
         population = []
@@ -208,12 +376,19 @@ class RHEA_agent:
         old_score = -2
         moves = 0
         reinf_cards_played = 0
+        time_elapsed = 0
+        time_per_move = 0
+        max_time = 20
+        self.actions = self.get_possible_moves(owned_countries, map)
+        state = owned_countries
+
         # initialize population
         for i in range(self.populationSize):
             population.append(self.OneSLA(map, owned_countries))
             population_scores.append(self.evaluateIndividual(population[i], map, owned_countries))
 
-        while new_score > old_score and moves <= self.maxMoves:
+        while new_score > old_score and moves <= self.maxMoves and ((max_time - time_elapsed) > time_per_move):
+            self.actions = self.get_possible_moves(owned_countries, map)
             moves += 1
             old_score = new_score
             # while time not elapsed:
@@ -231,7 +406,12 @@ class RHEA_agent:
 
             #print('BEST ACTION', best_inidvidual[0])
             all_moves.append(best_inidvidual[0])
-            self.performAction(best_inidvidual[0], owned_countries)
+            self.performAction(best_inidvidual[0], owned_countries, map,  owned_countries)
+
+            #updated time
+            time_elapsed = time.clock() - t0
+            time_per_move = time_elapsed / moves
+
         print(str(self.colour) + ' does these moves: ' + str(all_moves))
         return all_moves, reinf_cards_played, owned_countries
 
